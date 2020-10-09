06ce06875ddde441b302fe6150f103918972cd34
Philipp Lypniakov <filipprayden@gmail.com>
2020-10-10 00:27:35 +0300

    feat: add user mock-data

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,18 @@
+const mockdata = require('mockdata');
+const User = require('./user.model');
+
+const users = new Array(10);
+
+for (let i = 0; i < users.length; i++) {
+  users[i] = new User({
+    name: mockdata.name(),
+    login: `${mockdata.name()}${mockdata.chars(1, 5)}`,
+    password: `${mockdata.chars(5, 10)}`
+  });
+}
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return users;
 };
 
 module.exports = { getAll };
